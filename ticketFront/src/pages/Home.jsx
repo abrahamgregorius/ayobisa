3c64826624c0f43a71c118b6b139b65abe064ea2
Abraham <athioii6@gmail.com>
2023-12-15 12:38:48 +0700

    react update

--- ticketFront/src/pages/Home.jsx
+++ ticketFront/src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
             <div className="hero">
                 <div className="container">
                     <div className="hero-head">
-                        <h1>Hello world!</h1>
+                        <h1>Welcome to AyoTicket!</h1>
                     </div>
                     <div className="hero-body">
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod nulla dicta vitae officia, corrupti ex rem? Iusto, enim nisi tenetur minima labore similique omnis illum?</p>
